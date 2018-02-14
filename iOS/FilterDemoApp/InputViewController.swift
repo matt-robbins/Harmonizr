e29b364d026d221c5d93db6befe268a2017bc100
Matthew E Robbins <matt@matthews-macbook-pro.local>
2018-02-14 17:27:16 -0500

    input gain control working

--- iOS/FilterDemoApp/InputViewController.swift
+++ iOS/FilterDemoApp/InputViewController.swift
@@ -23,8 +23,11 @@ public class InputViewController: UIViewController, UITableViewDelegate, UITable
     
     @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var inputTable: UITableView!
-    var data: [AVAudioSessionPortDescription] = [AVAudioSessionPortDescription]()
+    @IBOutlet weak var inputDataSourceTable: UITableView!
+    @IBOutlet weak var inputGainSlider: UISlider!
     
+    var data: [AVAudioSessionPortDescription] = [AVAudioSessionPortDescription]()
+    var dataSources: [AVAudioSessionDataSourceDescription] = [AVAudioSessionDataSourceDescription]()
     
     @IBAction func done(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
@@ -32,34 +35,79 @@ public class InputViewController: UIViewController, UITableViewDelegate, UITable
     
     //MARK: UITableView protocol
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        
+        if (tableView === inputTable)
+        {
+            return data.count
+        }
+        
+        if (tableView === inputDataSourceTable)
+        {
+            return dataSources.count
+        }
+        
+        fatalError("gaaaaa!")
     }
     
     public func tableView(_ tableView: UITableView, numberOfSections section: Int) -> Int {
         return 1
     }
     
-    public func tableView(_ tableView: UITableView,
-                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        cell.textLabel?.text = data[indexPath.row].portName
-        return cell
+        if (tableView === inputTable)
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            
+            cell.textLabel?.text = data[indexPath.row].portName
+            return cell
+        }
+        if (tableView === inputDataSourceTable)
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dataSourceCell", for: indexPath)
+            
+            cell.textLabel?.text = dataSources[indexPath.row].dataSourceName
+            return cell
+        }
+        
+        fatalError("gaaaa!")
     }
 
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let row = (indexPath as NSIndexPath).row
         
-        let session = AVAudioSession.sharedInstance()
-        do {
-            try session.setPreferredInput(data[row])
-        }
-        catch {
-            print("oopsie!")
+        if (tableView === inputTable)
+        {
+            let session = AVAudioSession.sharedInstance()
+            do {
+                try session.setPreferredInput(data[row])
+            }
+            catch {
+                print("oopsie!")
+            }
+            
+            updateGainSlider()
+//            dataSources = AVAudioSession.sharedInstance().inputDataSources?
+//
+//            if (dataSources != nil)
+//            {
+//                self.inputDataSourceTable.reloadData()
+//            }
         }
         
         print(row)
         return
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func gainChanged(_ sender: UISlider) {
+        do {
+            try AVAudioSession.sharedInstance().setInputGain(sender.value)
+        }
+        catch {
+            
+        }
     }
     
     public override func viewDidLoad()
@@ -83,9 +131,22 @@ public class InputViewController: UIViewController, UITableViewDelegate, UITable
         }
         self.inputTable.reloadData()
         
+        updateGainSlider()
+        
         let indexPath = IndexPath(row: current_row, section: 0);
         self.inputTable.selectRow(at: indexPath, animated: false, scrollPosition: UITableViewScrollPosition.none)
         //self.tableView(self.tableView, didSelectRowAt: indexPath)
+    }
+    
+    
+    private func updateGainSlider()
+    {
+        let session = AVAudioSession.sharedInstance()
+        self.inputGainSlider.isEnabled =  session.isInputGainSettable
+        if (session.isInputGainSettable)
+        {
+            inputGainSlider.value = session.inputGain
+        }
     }
 }
 
